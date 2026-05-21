1778022d7d0752eed728644c30569ef053e1436e
Ravi Kumar <officialravikumar.tach@gmail.com>
2026-05-21 11:35:09 +0530

    Updated vite config and build files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.AI_Image_Generator  || "/AI_Image_Generator",
+  base: '/AI_Image_Generator/',
 })
